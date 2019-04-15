19ae7da32f7093aa5eee7a10db4f7cadaa7f9612
Carlos Valdez <33138184+DJCarlosValdez@users.noreply.github.com>
2019-04-15 11:47:57 -0500

    Method challenge

--- ruby/challenges/c1.rb
+++ ruby/challenges/c1.rb
@@ -1,5 +1,17 @@
-puts "Escribe un nombre:"
-name = gets.chomp
-letters = name.size()
-message = "Welcome #{name}, your name has #{letters} letters" 
-p message == "Welcome Rodrigo, your name has 7 letters"
+#puts "Escribe un nombre:"
+#name = gets.chomp
+#letters = name.size()
+#message = "Welcome #{name}, your name has #{letters} letters" 
+#p message == "Welcome Rodrigo, your name has 7 letters"
+
+def sum_strings (str1, str2)
+    str1 + str2
+end
+
+puts "Primer String"
+string1 = gets.chomp
+
+puts "Segundo String"
+string2 = gets.chomp
+
+p sum_strings(string1, string2)
