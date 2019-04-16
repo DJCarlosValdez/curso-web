d67bb2ad95264192ebd984525a1d4f601c6e73d5
Carlos Valdez <33138184+DJCarlosValdez@users.noreply.github.com>
2019-04-16 13:23:46 -0500

    Added Challenge 2, Class 2, And renamed "prueba" to "class1"

---
+++ ruby/challenges/c2.rb
@@ -0,0 +1,123 @@
+# def large (s) 
+#     if s.size > 20 
+#         s.upcase!
+#     else
+#         s
+#     end
+# end
+
+# p large("1234567890asdfghklñvcbndfghjkcvbn")
+# p large("abc")
+
+# -------------------------------------------------------------------------------------------------------------------------------
+
+# def shipping(address) 
+#     if address.upcase.include? "MEXICO"
+#         "Order Received"
+#     else
+#         "We only ship orders to Mexico"
+#     end
+# end
+# p shipping("Mexico")
+# p shipping("abc")
+
+# p shipping('Insurgentes Sur 8932, Alvaro Obregon, Mexico') == "Order Received"
+# p shipping('Geary Blvd 3320, San Francisco, Estados Unidos') == "We only ship orders to Mexico"
+
+# -------------------------------------------------------------------------------------------------------------------------------
+
+# def user ()
+#     inputs = 0
+#     passed_string = ""
+#     loop do
+#         passed_string = gets.chomp
+#         inputs += 1
+#         break if passed_string == "Ya"
+#     end 
+#     p inputs
+# end
+
+# user()
+
+# def user(input)
+# 	counter = 0 
+# 	while input != "Ya" do
+# 		p counter += 1
+# 		puts "Dentro de while"
+# 		puts "Ingresa un valor"
+# 		input = gets.chomp
+# 	end
+# 	p "Counter es igual a: #{counter}"
+# end
+
+# puts "Ingresa un valor"
+# p input = gets.chomp
+
+# user(input)
+
+# -------------------------------------------------------------------------------------------------------------------------------
+
+def create_array ()
+    inputs = 0
+    passed_string = ""
+    array = []
+    loop do
+        puts "Ingresa un strig para el array"
+        passed_string = gets.chomp
+        break if passed_string == "Ya"
+        p array << passed_string
+        p inputs += 1
+    end 
+    p inputs
+    array
+end
+
+
+array1 = create_array()
+array2 = []
+
+
+def order_array (array1, array2)
+    array1.each do |x|
+        x << "##{x.size}"
+    end
+    p array1
+    i=1
+    loop do 
+        array1.each_with_index do |x, index|
+            p index
+            p x
+            puts "dentro each array1"
+            if x.include? "##{i}"
+                puts "dentro if each array1"
+                puts "item split"
+                p item = x.split("#")
+                puts "item"
+                p item
+                puts "array2"
+                p array2 << item[0]
+            end
+            puts "Array 1: #{array1}"
+            p array2
+        end
+        i += 1
+        p i
+        break if array1.size == array2.size
+    end
+    array2
+end
+
+def shortest(a1, a2) 
+    puts a1
+    puts a2
+    array3 = order_array(a1, a2)
+    final_array = []
+    array3.each do |x|
+        if x.size == array3[0].size
+            final_array << x
+        end
+    end
+    p final_array
+end
+
+shortest(array1, array2)
