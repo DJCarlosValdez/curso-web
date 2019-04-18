ab70be5dee043e1be9f1a5257ffe25d9bb599065
Carlos Valdez <33138184+DJCarlosValdez@users.noreply.github.com>
2019-04-18 13:34:32 -0500

    Updated Challenges 4 and Class 4
    Classes challenges with private and protected objects

--- ruby/challenges/c4.rb
+++ ruby/challenges/c4.rb
@@ -58,38 +58,182 @@
 
 # --------------------------------------- Guessing Game Challenge -------------------------------------------------------------
 
-class GuessingGame
-    def initialize
-        puts "¡Bienvenido a Number Guessing Game!"
-        puts "¡Ingresa el número minimo para adivinar!"
-        @min = gets.chomp.to_i
-        puts "¡Ingresa el número máximo para adivinar!"
-        @max = gets.chomp.to_i
-        puts "¿En cuántos intentos lo quieres lograr?"
-        @limit = gets.chomp.to_i
+# class GuessingGame
+#     def initialize
+#         puts "¡Bienvenido a Number Guessing Game!"
+#         puts "¡Ingresa el número minimo para adivinar!"
+#         @min = gets.chomp.to_i
+#         puts "¡Ingresa el número máximo para adivinar!"
+#         @max = gets.chomp.to_i
+#         puts "¿En cuántos intentos lo quieres lograr?"
+#         @limit = gets.chomp.to_i
+#     end
+
+#     def guess 
+#         tries = 0
+#         input = 0
+#         number_to_guess = rand(@min..@max)
+#         loop do
+#             puts "¿Qué número crees que es?"
+#             input = gets.chomp.to_i
+#             tries += 1
+#             if tries == @limit
+#                 puts "¡Perdiste! El número era: #{number_to_guess}"
+#                 break
+#             elsif input == number_to_guess
+#                 puts "¡Ganaste! Lo lograste en: #{tries}/#{@limit} intentos."
+#                 break
+#             else
+#                 puts "¡Incorrecto! Te quedan #{@limit - tries} intentos."
+#             end
+#         end
+#     end
+# end
+
+# game = GuessingGame.new
+
+# game.guess
+
+# --------------------------------------- Person Age Challenge -------------------------------------------------------------
+
+# class Person
+#     attr_reader :age
+    
+#     def  initialize(age=0)
+#         @@age = age
+#     end
+  
+#     def age=(age)
+#         p @age = age
+#         p @@age = age
+#     end
+  
+#     #no modificar este método de clase
+#     def self.birthday
+#         p @@age
+#       @@age += 1
+#     end
+# end
+  
+
+# alice = Person.new(7)
+
+# #test
+# p alice.age = 17
+# p alice.age == 17
+# # #=> true
+# #p alice.@age 
+# p Person.birthday == 18
+
+# --------------------------------------- Heritage Challenge -------------------------------------------------------------
+
+# class Animal
+#     def speak
+#         "Hello!"
+#     end
+# end
+
+# class Dog < Animal
+# end
+
+# class Cat < Animal
+# end
+
+# drako = Dog.new
+# peto = Cat.new
+# p drako.speak == "Hello!"          
+# # => true
+# p peto.speak == "Hello!"           
+# # => true
+
+# --------------------------------------- Heritage 2 Challenge -------------------------------------------------------------
+
+# class Animal
+#     def initialize (name = "Pet")
+#         @name = name
+#     end
+
+#     def speak
+#         "Hello!"
+#     end
+    
+# end
+
+# class Dog < Animal
+
+#     def speak
+#         "#{@name} say Guau!"
+#     end
+# end
+
+# class Cat < Animal
+# end
+
+# #test
+# drako = Dog.new("Drako")
+# peto = Cat.new
+# p drako.speak == "Drako say Guau!"         
+# # => true
+# p peto.speak == "Hello!"           
+# # => true
+
+# --------------------------------------- Heritage 3 Challenge -------------------------------------------------------------
+
+# class Vehicle
+#     @@number_of_vehicles = 0
+
+#     def initialize
+#         p self
+#         Vehicle.number_of_vehicles
+#     end
+
+    
+#     def self.number_of_vehicles
+#         p @@number_of_vehicles += 1
+#         p"This Superclass has created #{@@number_of_vehicles} vehicles"
+#     end
+# end
+
+# class Car < Vehicle
+
+# end
+
+# class Bus < Vehicle
+
+# end
+
+# #test
+# car_1 = Car.new
+# car_2 = Car.new
+# bus_1 = Bus.new
+# p Car.number_of_vehicles == "This Superclass has created 3 vehicles"
+
+# --------------------------------------- Private Challenge -------------------------------------------------------------
+
+class Person
+    # attr_reader(:age)
+    def initialize (name, born_year)
+        @name = name
+        @born_year = born_year
     end
 
-    def guess 
-        tries = 0
-        input = 0
-        number_to_guess = rand(@min..@max)
-        loop do
-            puts "¿Qué número crees que es?"
-            input = gets.chomp.to_i
-            tries += 1
-            if tries == @limit
-                puts "¡Perdiste! El número era: #{number_to_guess}"
-                break
-            elsif input == number_to_guess
-                puts "¡Ganaste! Lo lograste en: #{tries}/#{@limit} intentos."
-                break
-            else
-                puts "¡Incorrecto! Te quedan #{@limit - tries} intentos."
-            end
-        end
+    def age 
+        years_old
+        p"#{@name} is #{@age} years old"
+    end
+
+    private 
+
+    def years_old
+        p t = Time.new.year
+        @age = t - @born_year
     end
 end
 
-game = GuessingGame.new
+carlos = Person.new("Carlos",1989)
+martha = Person.new("Martha",1993)
 
-game.guess
+p carlos.age == "Carlos is 30 years old."
+#=>true
+p martha.age == "Martha is 26 years old."
+#=>true
